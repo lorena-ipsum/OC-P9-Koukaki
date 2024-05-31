76a8d0520ac9769adbecb53e76561446b05fa3f5
lorena-ipsum <lorenanbo@gmail.com>
2024-05-31 14:58:30 +0200

    Debut de Projet

---
+++ functions.php
@@ -0,0 +1,25 @@
+<?php
+add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
+function theme_enqueue_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+
+// Get customizer options from parent theme
+if ( get_stylesheet() !== get_template() ) {
+    add_filter( 'pre_update_option_theme_mods_' . get_stylesheet(), function ( $value, $old_value ) {
+        update_option( 'theme_mods_' . get_template(), $value );
+        return $old_value; // prevent update to child theme mods
+    }, 10, 2 );
+    add_filter( 'pre_option_theme_mods_' . get_stylesheet(), function ( $default ) {
+        return get_option( 'theme_mods_' . get_template(), $default );
+    } );
+}
+
+function my_theme_enqueue_scripts() {
+    // Enquêter jQuery inclus avec WordPress
+    wp_enqueue_script('jquery');
+
+    // Enquêter votre propre fichier JavaScript
+    wp_enqueue_script('my-theme-script', get_stylesheet_directory_uri() . '/js/my-script.js', array('jquery'), null, true);
+}
+add_action('wp_enqueue_scripts', 'my_theme_enqueue_scripts');
