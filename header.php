954899a16be99172974f019b062598ac6c4e85b1
lorena-ipsum <lorenanbo@gmail.com>
2024-06-17 17:39:51 +0200

    Final Modifications

--- header.php
+++ header.php
@@ -11,35 +11,39 @@
 <div id="page" class="site">
     <a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'foce' ); ?></a>
 
-    <header id="masthead" class="site-header">
-        <nav id="site-navigation" class="main-navigation">
-            <div class="nav-header">
-                <li class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></li>
-                <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/burger.png'); ?>" class="fa-bars" id="open-menu" alt="Menu" />
-                <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/burger_croix.png'); ?>" class="fa-xmark hidden" id="close-menu" alt="Menu X" />
-            </div>
-            <div id="primary-menu" class="menu hidden">
-                <ul>
-                    <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/logomini.png'); ?>" alt="logo Fleurs d'oranger & chats errants">
-                    <li><a class="section--titre__focus" href="#story">Histoire</a></li>
-                    <li><a class="section--titre__focus" href="#characters">Personnages</a></li>
-                    <li><a class="section--titre__focus" href="#place">Lieu</a></li>
-                    <li><a class="section--titre__focus" href="#studio">Studio Koukaki</a></li>
-                </ul>
-                <div>
-                    <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/catblack.png'); ?>" class= "catblack" alt="tete de chat noir" />
-                    <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/catorage.png'); ?>" class= "catorange" alt="tete de chat orange" />
-                    <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/catpurple.png'); ?>" class= "catpurple" alt="tete de chat purple" />
-                    <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/flowermenu.png'); ?>" class= "flowermenu" alt="fleur du menu" />
-                    <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/flowermenu2.png'); ?>" class= "flowermenu2" alt="fleur du menu" />
-                    <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/flowermenu3.png'); ?>" class= "flowermenu3" alt="fleur du menu" />
-                    <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/flowermenu4.png'); ?>" class= "flowermenu4" alt="fleur du menu" />
-                    <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/hibiscusmenu.png'); ?>" class= "hibiscusmenu" alt="fleur du menu" />
-                    <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/StudioKoukaki.png'); ?>" class= "StudioKoukaki" alt="fleur du menu" />
+    <div class="site-header-wrapper">
+        <header id="masthead" class="site-header">
+            <nav id="site-navigation" class="main-navigation">
+                <div class="nav-header">
+                    <li class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></li>
+                    <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/burger.png'); ?>" class="fa-bars" id="open-menu" alt="Menu" />
+                    <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/burger_croix.png'); ?>" class="fa-xmark hidden" id="close-menu" alt="Menu X" />
                 </div>
-            </div>
-        </nav>
-    </header>
+                <div class="menu-wrapper">
+                    <div id="primary-menu" class="menu hidden">
+                        <ul>
+                            <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/logomini.png'); ?>" alt="logo Fleurs d'oranger & chats errants">
+                            <li><a class="section--titre__focus" href="#story">Histoire</a></li>
+                            <li><a class="section--titre__focus" href="#characters">Personnages</a></li>
+                            <li><a class="section--titre__focus" href="#place">Lieu</a></li>
+                            <li><a class="section--titre__focus" href="#studio">Studio Koukaki</a></li>
+                        </ul>
+                        <div>
+                            <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/catblack.png'); ?>" class= "catblack" alt="tete de chat noir" />
+                            <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/catorage.png'); ?>" class= "catorange" alt="tete de chat orange" />
+                            <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/catpurple.png'); ?>" class= "catpurple" alt="tete de chat purple" />
+                            <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/flowermenu.png'); ?>" class= "flowermenu" alt="fleur du menu" />
+                            <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/flowermenu2.png'); ?>" class= "flowermenu2" alt="fleur du menu" />
+                            <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/flowermenu3.png'); ?>" class= "flowermenu3" alt="fleur du menu" />
+                            <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/flowermenu4.png'); ?>" class= "flowermenu4" alt="fleur du menu" />
+                            <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/hibiscusmenu.png'); ?>" class= "hibiscusmenu" alt="fleur du menu" />
+                            <img src="<?php echo esc_url(get_stylesheet_directory_uri() . '/assets/images/StudioKoukaki.png'); ?>" class= "StudioKoukaki" alt="fleur du menu" />
+                        </div>
+                    </div>
+                </div>
+            </nav>
+        </header>
+    </div>
 </div>
 <?php wp_footer(); ?>
 </body>
